0781212e60136ae082e9703177b2069c1ad08154
MAL1S <karbushev_ivan@mail.ru>
2024-05-02 23:50:47 +0300

    restrict scrolling ZoomableImage up to h/w corners (but need optimizations and handle zoom-out case)

--- common_compose/build.gradle.kts
+++ common_compose/build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
 
     implementation("com.google.code.gson:gson:2.10.1")
 
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
     debugImplementation("androidx.compose.ui:ui-tooling:1.6.1")
 
     implementation("androidx.core:core-ktx:1.12.0")
--- common_compose/src/main/java/com/example/common/ZoomableImage.kt
+++ common_compose/src/main/java/com/example/common/ZoomableImage.kt
@@ -10,9 +10,12 @@ import androidx.compose.foundation.gestures.calculatePan
 import androidx.compose.foundation.gestures.calculateZoom
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -20,15 +23,17 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import coil.compose.AsyncImagePainter
 import kotlinx.coroutines.awaitCancellation
 import kotlinx.coroutines.launch
+import kotlin.math.abs
 
 @Composable
 fun ZoomableImage(
-    painter: Painter,
+    painter: AsyncImagePainter,
     modifier: Modifier = Modifier,
     backgroundColor: Color = Color.Transparent,
     imageAlign: Alignment = Alignment.Center,
@@ -41,9 +46,13 @@ fun ZoomableImage(
 ) {
     val coroutineScope = rememberCoroutineScope()
 
-    val scale = remember { mutableFloatStateOf(1f) }
+    var scale by remember { mutableStateOf(1f) }
     val offsetX = remember { mutableFloatStateOf(1f) }
     val offsetY = remember { mutableFloatStateOf(1f) }
+
+    val displayMetrics = LocalContext.current.resources.displayMetrics
+    val widthPix = displayMetrics.widthPixels
+    val heightPix = displayMetrics.heightPixels
 
     Box(
         modifier = Modifier
@@ -55,25 +64,51 @@ fun ZoomableImage(
                         awaitFirstDown()
                         do {
                             val event = awaitPointerEvent()
-                            scale.floatValue *= event.calculateZoom()
-                            if (scale.floatValue > 1) {
+                            val calculatedZoom = event.calculateZoom()
+                            if (scale * calculatedZoom in 1f..3f) {
+                                scale *= event.calculateZoom()
+                            }
+                            if (scale in 1f..3f) {
                                 scrollState?.run {
                                     coroutineScope.launch {
                                         setScrolling(false)
                                     }
                                 }
                                 val offset = event.calculatePan()
-                                offsetX.floatValue += offset.x
-                                offsetY.floatValue += offset.y
+                                val calculatedX = abs(offsetX.floatValue + offset.x)
+                                val calculatedY = abs(offsetY.floatValue + offset.y)
+
+                                var imageHeight = 1
+                                var imageWidth = 1
+                                var imageInitialScale = 1.0
+
+                                (painter.state as? AsyncImagePainter.State.Success)?.painter?.let {
+                                    imageHeight = it.intrinsicSize.height.toInt()
+                                    imageWidth = it.intrinsicSize.width.toInt()
+                                    imageInitialScale = widthPix * 1.0 / imageWidth
+                                }
+
+                                val shouldScrollY = (heightPix - scale * imageInitialScale * imageHeight) < 0
+                                val shouldScrollX = (widthPix - scale * imageWidth * imageWidth) < 0
+
+                                val koefX = (scale - 1) / 2
+                                val yMinFilledScale = heightPix / (imageHeight * imageInitialScale)
+                                val koefY = (scale - yMinFilledScale) / 2
+                                val maxX = widthPix * koefX
+                                val maxY = imageHeight * imageInitialScale * koefY
+
+                                if (calculatedX <= maxX && shouldScrollX) {
+                                    offsetX.floatValue += offset.x
+                                }
+                                if (calculatedY < maxY && shouldScrollY) {
+                                    offsetY.floatValue += offset.y
+                                }
+
                                 scrollState?.run {
                                     coroutineScope.launch {
                                         setScrolling(true)
                                     }
                                 }
-                            } else {
-                                scale.floatValue = 1f
-                                offsetX.floatValue = 1f
-                                offsetY.floatValue = 1f
                             }
                         } while (event.changes.any { it.pressed })
                     }
@@ -89,8 +124,8 @@ fun ZoomableImage(
                 .align(imageAlign)
                 .graphicsLayer {
                     if (isZoomable) {
-                        scaleX = maxOf(maxScale, minOf(minScale, scale.floatValue))
-                        scaleY = maxOf(maxScale, minOf(minScale, scale.floatValue))
+                        scaleX = maxOf(maxScale, minOf(minScale, scale))
+                        scaleY = maxOf(maxScale, minOf(minScale, scale))
 
                         translationX = offsetX.floatValue
                         translationY = offsetY.floatValue
